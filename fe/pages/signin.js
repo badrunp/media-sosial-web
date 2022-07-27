6b089fa85056903df9c475907e20bc86364ff2b0
= <bbadrunn@gmail.com>
2022-07-27 17:40:01 +0700

    test deploy to vercel

--- fe/pages/signin.js
+++ fe/pages/signin.js
@@ -48,7 +48,7 @@ export default function Signin() {
               type="text"
               name="email"
               id="email"
-              className="tracking-wide border border-zinc-300 rounded-full w-full focus:outline-none px-6 py-3 mt-1 placeholder:font-light placeholder:text-zinc-400 text-sm sm:text-base focus:ring-violet-200 transition-colors ease-in-out"
+              className="tracking-wide border border-zinc-400/70 rounded-full w-full focus:outline-none px-6 py-3 mt-1 placeholder:font-light placeholder:text-zinc-500 text-sm sm:text-base focus:ring-2 focus:border-violet-600 focus:ring-violet-200 transition-colors ease-in-out"
               autoComplete="off"
               placeholder="your@gmail.com"
               onChange={handleChangeInput}
@@ -65,7 +65,7 @@ export default function Signin() {
               type="text"
               name="password"
               id="password"
-              className="tracking-wide border border-zinc-300 rounded-full w-full focus:outline-none px-6 py-3 mt-1 placeholder:font-light placeholder:text-zinc-400 text-sm sm:text-base focus:ring-violet-200 transition-colors ease-in-out"
+              className="tracking-wide border border-zinc-400/70 rounded-full w-full focus:outline-none px-6 py-3 mt-1 placeholder:font-light placeholder:text-zinc-500 text-sm sm:text-base focus:ring-2 focus:border-violet-600 focus:ring-violet-200 transition-colors ease-in-out"
               autoComplete="off"
               placeholder="Min. 6 character"
               onChange={handleChangeInput}
@@ -76,7 +76,7 @@ export default function Signin() {
             <div className="flex items-center space-x-2">
               <input
                 type="checkbox"
-                className="rounded-sm border-zinc-300 cursor-pointer text-violet-600 focus:ring-violet-600 block font-semibold text-sm sm:text-base"
+                className="rounded-sm border-zinc-400/70 cursor-pointer text-violet-600 focus:ring-violet-600 block font-semibold text-sm sm:text-base"
                 id="remember"
               />
               <label
@@ -103,15 +103,15 @@ export default function Signin() {
           </button>
         </form>
 
-        <div className="mt-8 border-t border-zinc-300 pt-8 relative">
-          <span className="absolute -top-3 text-sm left-1/2 -translate-x-1/2 bg-white px-4 font-normal text-zinc-400 tracking-wide truncate">
+        <div className="mt-8 border-t border-zinc-400/70 pt-8 relative">
+          <span className="absolute -top-3 text-sm left-1/2 -translate-x-1/2 bg-white px-4 font-normal text-zinc-500 tracking-wide truncate">
             or Sign in with Email
           </span>
           <button
             type="button"
-            className="w-full space-x-3 px-6 py-3 border border-zinc-300 rounded-full font-semibold text-sm sm:text-base flex items-center justify-center"
+            className="w-full space-x-3 px-6 py-3 border border-zinc-400/70 rounded-full font-semibold text-sm sm:text-base flex items-center justify-center"
           >
-            <Image src={"/images/google.png"} width="28" height="28" />
+            <Image src={"/images/google.png"} width="25" height="25" />
             <span>Sign in with Google</span>
           </button>
         </div>
